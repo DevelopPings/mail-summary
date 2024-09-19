9993c1e62b436ee54b7473093c54a7893ec0d01d
FJHGF <fjhgf@naver.com>
2024-09-20 03:16:28 +0900

    Add open AI API code

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -1,41 +1,61 @@
 // 우클릭 메뉴 생성
-const menuId = 'Whale-Mail';
-chrome.contextMenus.remove(menuId, () => {
+// const menuId = 'Whale-Mail';
+// console.log(menuId);
+chrome.runtime.onInstalled.addListener(() => {
 	chrome.contextMenus.create({
-		id: menuId,
+		id: 'Whale-Mail',
 		title: '웨-일이 쉽지? (Whale Mail)', // 메뉴에 보이는 글자
 		contexts: ['all'],
 	});
 });
 // content.js로 크롤링
-chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		files: ['assets/scripts/content.js'],
-	});
+// chrome.action.onClicked.addListener((tab) => {
+// 	console.log(tab.id);
+// 	chrome.scripting.executeScript({
+// 		target: { tabId: tab.id },
+// 		files: ['assets/scripts/content.js'],
+// 	});
+// });
+// 크롤링 관련 메시지 처리
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+// 	if (message.action === 'crawlMail') {
+// 		chrome.tabs.sendMessage(sender.tab.id, { action: 'Whale-Mail' });
+// 	}
+// });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.menuItemId === 'Whale-Mail') {
+		chrome.scripting
+			.executeScript({
+				target: { tabId: tab.id },
+				files: ['assets/scripts/content.js'],
+			})
+			.then(() => {
+				chrome.tabs.sendMessage(tab.id, {
+					action: 'Whale-Mail',
+					selectedText: info.selectionText,
+				});
+			})
+			.catch((err) => console.warn('unexpected error', err));
+	}
 });
-
 let chatGPTResponse = '';
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.type === 'greeting') {
+	if (request.type === 'summaryMail') {
 		chatGPTResponse = request.payload.message;
 		console.log(
 			// chatgpt가 쓴거 나옴
 			chatGPTResponse,
 		);
 		// 후속 작업을 처리하는 함수 호출
-		handleMessage(chatGPTResponse);
-
-		const returnMessage = `방가워~ 이것은 contentScript의 메시지를 background에서 보내는 응답 메시지야~`;
-		sendResponse({
-			message: returnMessage,
-		});
+		todoMessage(chatGPTResponse);
 	}
 	return true; // 비동기로 작업 시 필요
 });
-
-function handleMessage(message) {
+function todoMessage(message) {
 	// message를 활용하는 후속 작업
-	console.log('Message for handling:', message);
-	document.querySelector('#test').innerHTML = chatGPTResponse;
+	console.log(message);
+	const todo = document.querySelector('#test');
+	todo.innerHTML = message;
+
+	// 2개로 나누기
 }
--- assets/scripts/content.js
+++ assets/scripts/content.js
@@ -2,7 +2,11 @@ const CHATGPT_API_KEY =
 	'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'; // <- 절대 레포에 올리지 마세요
 const CHATGPT_MODEL = 'gpt-4o-mini';
 const prompt =
-	'메일 내용을 최소 1개 ~ 최대 5줄로 요약하고 각 줄은 최소 10글자에서 최대 50글자로 요약해야 한다. 메일을 todo로 만들어주는데 최소 0개에서 최대 10개로 만들어주고 최소 10글자에서 최대 50글자 이내로 요약해야한다. todo와 summary는 날짜, 시간, 전화번호, 숫자, 필수, 해주세요, 가능, 마감, 부탁, 안내, 일시, 장소, 주소, 진행, 방법, 확인이라는 글자가 있는 문구가 있는 내용은 중요한 내용으로 생각하고 필수로 넣어서 요약하거나 todo로 출력해야한다. 장소, 주소는 요약하지 말고 그대로 넣어서 요약해야한다. 요약은 summary : 로 시작하고, todo는 todo로 시작해서 출력한다.';
+	'메일 내용을 최소 1개 ~ 최대 5줄로 요약하고 각 줄은 최소 10글자에서 최대 50글자로 요약해야 한다.' +
+	'메일을 todo로 만들어주는데 최소 0개에서 최대 10개로 만들어주고 최소 10글자에서 최대 50글자 이내로 요약해야한다.' +
+	'todo와 summary는 이름, 날짜, 시간, 전화번호, 숫자, 필수, 해주세요, 가능, 마감, 부탁, 안내, 일시, 장소, 주소, 진행, 방법, 확인이라는 글자가 있는 문구가 있는 내용은 중요한 내용으로 생각하고 필수로 넣어서 요약하거나 todo로 출력해야한다.' +
+	'장소, 주소는 요약하지 말고 그대로 넣어서 요약해야한다. 요약은 [summary] 로 머릿말을 시작하고, todo는 [todo]로 머릿말을시작한다.' +
+	'각 줄 앞에는 숫자로 표시해서 출력한다.';
 
 async function callChatGPT(question) {
 	try {
@@ -36,7 +40,6 @@ async function callChatGPT(question) {
 		console.error(error.message);
 	}
 }
-
 function crawlContent() {
 	return new Promise((resolve, reject) => {
 		let mail = {
@@ -47,22 +50,27 @@ function crawlContent() {
 		};
 		setTimeout(() => {
 			try {
-				if (location.host.includes('mail.naver.com')) {
-					// 네이버 메일 크롤링 로직
-					const mailTitle =
-						document.querySelector(
-							'.text > span > span:nth-child(2)',
-						)?.innerHTML || '';
-					const mailSend =
-						document.querySelector('.option_area > button')
-							?.innerHTML || '';
+				if (location.host == 'mail.naver.com') {
+					// 네이버
+					// 제목
+					const mailTitle = document.querySelectorAll(
+						'.text > span > span:nth-child(2)',
+					)[0].innerHTML;
+
+					// 보낸 사람
+					const mailSend = document.querySelectorAll(
+						'.option_area > button',
+					)[0].innerHTML;
+
+					// 시간
 					const mailTime =
-						document.querySelector('.info_area > span')
-							?.innerHTML || '';
-					const mailContent =
-						document
-							.querySelector('.mail_view_contents')
-							?.innerText.trim() || '';
+						document.querySelectorAll('.info_area > span')[0]
+							.innerHTML;
+
+					// 내용
+					const mailContent = document
+						.querySelectorAll('.mail_view_contents')[0]
+						.innerText.trim();
 
 					mail = {
 						title: mailTitle,
@@ -70,29 +78,57 @@ function crawlContent() {
 						time: mailTime,
 						content: mailContent,
 					};
-				} else if (location.host.includes('mail.google.com')) {
-					// 구글 메일 크롤링 로직
-					document.querySelector('.ajz')?.click();
-					document.querySelector('.ajz')?.click();
+				} else if (location.host == 'mail.google.com') {
+					// 구글
+					// 제목
+					document.querySelectorAll('.ajz')[0].click();
+					document.querySelectorAll('.ajz')[0].click();
 
-					let mailTitle =
-						document
+					let mailTitle;
+					const getTitle = () => {
+						mailTitle = document
 							.querySelectorAll('.ajv')[3]
-							?.querySelector('td:nth-child(2)')?.innerText || '';
-					let mailSend =
-						document
+							.querySelectorAll('td')[1].innerText;
+					};
+
+					// 보낸 사람
+
+					let mailSend;
+					const getSend = () => {
+						mailSend = document
 							.querySelectorAll('.ajv')[0]
-							?.querySelector('span.qu')?.innerText || '';
-					let mailTime =
-						document
+							.querySelectorAll('span.qu')[0]
+							.querySelector('span.gD')
+							.getAttribute('email');
+					};
+
+					// 시간
+					let mailTime;
+					const getTime = () => {
+						mailTime = document
 							.querySelectorAll('.ajv')[2]
-							?.querySelector('span:nth-child(2)')?.innerHTML ||
-						'';
-					let mailContent =
-						document
-							.querySelector('.a3s.aiL div div')
-							?.innerHTML.replace(/<\/?[^>]+(>|$)|\s+/g, '') ||
-						'';
+							.querySelectorAll('span')[1].innerHTML;
+					};
+
+					// 내용
+					let mailContent;
+					const getContent = () => {
+						mailContent =
+							document.querySelectorAll('.a3s.aiL div div')[0]
+								.innerHTML;
+						// 전달받은 메세지 태그였음
+						// let mailContentArray = document
+						// 	.querySelectorAll('.Bk div div div div.gs div div div')[2]
+						// 	.querySelectorAll('div div div span');
+						// for (let i = 0; i < mailContentArray.length; i++) {
+						// 	mailContent += mailContentArray[i].innerHTML;
+						// }
+					};
+
+					getTitle();
+					getSend();
+					getTime();
+					getContent();
 
 					mail = {
 						title: mailTitle,
@@ -113,14 +149,29 @@ chrome.runtime.onMessage.addListener(async (message) => {
 	if (message.action === 'Whale-Mail') {
 		try {
 			const result = await crawlContent();
-			// console.log(result.content);
+			// 메일 크롤링 결과
+			console.log(result.title);
+			console.log(result.send);
+			console.log(result.time);
+			console.log(result.content);
 			const chatGPTResponse = await callChatGPT(result.content);
-			// console.log(chatGPTResponse);
+			// AI 결과
+			console.log(chatGPTResponse);
 			chrome.runtime.sendMessage(
 				{
-					type: 'greeting',
+					type: 'summaryMail',
 					payload: {
-						message: chatGPTResponse,
+						message:
+							'[제목]' +
+							result.title +
+							',' +
+							'[보낸 사람]' +
+							result.send +
+							',' +
+							'[보낸 시간]' +
+							result.time +
+							',' +
+							chatGPTResponse,
 					},
 				},
 				(response) => {
@@ -129,13 +180,11 @@ chrome.runtime.onMessage.addListener(async (message) => {
 							'Error:',
 							chrome.runtime.lastError.message,
 						);
-						console.log(new Date());
 					} else {
 						console.log(
 							'message received from sendResponse: ' +
 								response.message,
 						);
-						console.log(new Date());
 					}
 				},
 			);

d2f097c20c82c540e35f780f56f8b15dc2fb5f8f
FJHGF <fjhgf@naver.com>
2024-10-06 14:59:17 +0900

    Fix : gmail time english version

--- assets/scripts/content.js
+++ assets/scripts/content.js
@@ -122,11 +122,61 @@ function crawlGoogleMail(mail) {
 		.querySelector('span.qu span.gD')
 		.getAttribute('email');
 
-	const timeContent = document.querySelector('.g3').textContent;
+	const timeContent = gmailTime();
 
 	mail.time = convertTime(timeContent);
 
 	mail.content = contentType();
+}
+
+function gmailTime() {
+	document.querySelectorAll('.ajz')[0].click();
+	document.querySelectorAll('.ajz')[0].click();
+
+	// 처음 받을 때는 2번째인데
+	let checktime = document
+		.querySelectorAll('.ajv')[2]
+		.querySelectorAll('span')[1].textContent;
+
+	if (monthCheck(checktime)) {
+		return checktime;
+	}
+
+	// 답장인 경우 3번째임
+	if (isNaN(checktime.charAt(0))) {
+		checktime = document
+			.querySelectorAll('.ajv')[3]
+			.querySelectorAll('span')[1].textContent;
+	}
+
+	return checktime;
+}
+
+function monthCheck(time) {
+	// 3글자 추출
+	const check = time.toLowerCase().substring(0, 3);
+
+	// 1월부터 12월까지
+	const months = [
+		'jan',
+		'feb',
+		'mar',
+		'apr',
+		'may',
+		'jun',
+		'jul',
+		'aug',
+		'sep',
+		'oct',
+		'nov',
+		'dec',
+	];
+
+	// 날짜가 맞는지 확인
+	if (months.includes(check)) {
+		return true;
+	}
+	return false;
 }
 
 function gmilText() {
@@ -203,6 +253,8 @@ function convertTime(timeString) {
 		.replace(/,+/g, ',')
 		.split(',')
 		.map(Number);
+
+	console.log('convertTime', timeParts);
 
 	const [year, month, day, hour, minute] = timeParts;
 	return `${year}-${month}-${day} ${(hour % 12) + (isAfternoon ? 12 : 0)}:${minute}`;
